20209982869faa59e87af416dbabb517c0e063af
Om Rajput <omrajputt369@gmail.com>
2026-06-23 11:19:38 +0530

    Add simple python program: 25_shuffle_cards.py

---
+++ 25_shuffle_cards.py
@@ -0,0 +1,9 @@
+# Shuffle Deck of Cards
+import itertools, random
+
+deck = list(itertools.product(range(1,14),['Spade','Heart','Diamond','Club']))
+random.shuffle(deck)
+
+print("You got:")
+for i in range(5):
+    print(f"{deck[i][0]} of {deck[i][1]}")
