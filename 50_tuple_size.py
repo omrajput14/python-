9593791a0ef450f777ab9e23ed53e330509ca1cf
Om Rajput <omrajputt369@gmail.com>
2026-06-24 22:14:06 +0530

    Add intermediate python program: 50_tuple_size.py

---
+++ 50_tuple_size.py
@@ -0,0 +1,7 @@
+# Find the size of a tuple
+import sys
+
+my_tuple = ("apple", "banana", "cherry", "date")
+
+print("Tuple:", my_tuple)
+print("Size of tuple:", str(sys.getsizeof(my_tuple)) + " bytes")
