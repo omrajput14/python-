e64a65674c0a290cc51a3773b854eb245100d278
Om Rajput <omrajputt369@gmail.com>
2026-06-24 22:13:58 +0530

    Add intermediate python program: 46_slice_lists.py

---
+++ 46_slice_lists.py
@@ -0,0 +1,8 @@
+# Slice lists
+my_list = [1, 2, 3, 4, 5]
+
+print("Original list:", my_list)
+print("Items from index 1 to 3:", my_list[1:4])
+print("Items from beginning to index 2:", my_list[:3])
+print("Items from index 2 to end:", my_list[2:])
+print("Items in reverse:", my_list[::-1])
