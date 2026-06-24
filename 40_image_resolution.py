0ef243249f7a13097d914624c4238aef3665a459
Om Rajput <omrajputt369@gmail.com>
2026-06-24 22:13:47 +0530

    Add intermediate python program: 40_image_resolution.py

---
+++ 40_image_resolution.py
@@ -0,0 +1,7 @@
+# Find the resolution of an image (simulated for simplicity)
+def jpeg_res(filename):
+    print("Function to find resolution")
+    return 1920, 1080
+
+width, height = jpeg_res("dummy.jpg")
+print(f"The resolution of the image is {width}x{height}")
