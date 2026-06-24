453d5c995dd5b33c7b0cb6def3ee7cc34b6968ac
Om Rajput <omrajputt369@gmail.com>
2026-06-24 22:14:04 +0530

    Add intermediate python program: 49_last_list_element.py

---
+++ 49_last_list_element.py
@@ -0,0 +1,6 @@
+# Get the last element of the list
+my_list = ['a', 'b', 'c', 'd', 'e']
+
+print("List:", my_list)
+print("Last element using negative indexing:", my_list[-1])
+print("Last element using pop():", my_list.pop())
