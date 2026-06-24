941f95a8529eb4161529e140c02c3b51231d094b
Om Rajput <omrajputt369@gmail.com>
2026-06-24 22:14:00 +0530

    Add intermediate python program: 47_parse_string_to_num.py

---
+++ 47_parse_string_to_num.py
@@ -0,0 +1,12 @@
+# Parse a string into a float or int
+balance_str = "1500"
+balance_int = int(balance_str)
+
+print(type(balance_int))
+print(balance_int)
+
+balance_float_str = "1500.4"
+balance_float = float(balance_float_str)
+
+print(type(balance_float))
+print(balance_float)
