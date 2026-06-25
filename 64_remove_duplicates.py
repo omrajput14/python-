d964053ff014ca5efc99f69fa29d6eedabb1e467
Om Rajput <omrajputt369@gmail.com>
2026-06-25 21:45:07 +0530

    Add intermediate python program: 64_remove_duplicates.py

---
+++ 64_remove_duplicates.py
@@ -0,0 +1,7 @@
+# Remove duplicates from a list
+my_list = [1, 2, 2, 3, 4, 4, 5]
+
+unique_list = list(set(my_list))
+
+print("Original list:", my_list)
+print("List without duplicates:", unique_list)
