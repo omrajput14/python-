be98a1b477d63305e1b1649ff6027883ee68eecd
Om Rajput <omrajputt369@gmail.com>
2026-06-25 21:45:07 +0530

    Add intermediate python program: 62_concatenate_lists.py

---
+++ 62_concatenate_lists.py
@@ -0,0 +1,9 @@
+# Concatenate two lists
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+
+concatenated = list1 + list2
+
+print("List 1:", list1)
+print("List 2:", list2)
+print("Concatenated:", concatenated)
