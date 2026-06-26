f336971fe071319695848df995ade8d17d3cadb2
Om Rajput <omrajputt369@gmail.com>
2026-06-26 18:20:40 +0530

    Add intermediate python program: 67_find_longest_word.py

---
+++ 67_find_longest_word.py
@@ -0,0 +1,7 @@
+# Find the longest word in a list
+words = ["apple", "banana", "watermelon", "kiwi", "strawberry"]
+
+longest_word = max(words, key=len)
+
+print("Words:", words)
+print("Longest word:", longest_word)
