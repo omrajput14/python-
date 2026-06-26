381629e4d4b11ba4ff66f4340b1960aab60ce4e3
Om Rajput <omrajputt369@gmail.com>
2026-06-26 18:20:40 +0530

    Add intermediate python program: 65_reverse_string.py

---
+++ 65_reverse_string.py
@@ -0,0 +1,9 @@
+# Reverse a string
+def reverse_string(s):
+    return s[::-1]
+
+my_string = "hello world"
+reversed_str = reverse_string(my_string)
+
+print("Original:", my_string)
+print("Reversed:", reversed_str)
