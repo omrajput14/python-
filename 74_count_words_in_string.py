8597ae103014915d96db3a091ae010d02dbfee61
Om Rajput <omrajputt369@gmail.com>
2026-06-26 18:20:42 +0530

    Add intermediate python program: 74_count_words_in_string.py

---
+++ 74_count_words_in_string.py
@@ -0,0 +1,8 @@
+# Count the number of words in a string
+text = "This is a sample string to count the number of words"
+
+word_list = text.split()
+word_count = len(word_list)
+
+print("Original text:", text)
+print("Number of words:", word_count)
