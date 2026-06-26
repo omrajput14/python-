d742d30d1257a42132598543403857efd8e1d854
Om Rajput <omrajputt369@gmail.com>
2026-06-26 18:20:42 +0530

    Add intermediate python program: 73_find_max_min.py

---
+++ 73_find_max_min.py
@@ -0,0 +1,9 @@
+# Find the maximum and minimum values in a dictionary
+my_dict = {'a': 100, 'b': 250, 'c': 50, 'd': 300}
+
+max_value = max(my_dict.values())
+min_value = min(my_dict.values())
+
+print("Dictionary:", my_dict)
+print("Maximum value:", max_value)
+print("Minimum value:", min_value)
