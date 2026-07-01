a916e7fd66fd867931f478e7b4e48ff06d696b5b
Om Rajput <omrajputt369@gmail.com>
2026-07-01 19:04:54 +0530

    Add Edit Distance DP

---
+++ 131_edit_distance.py
@@ -0,0 +1,22 @@
+def edit_distance(str1, str2, m, n):
+    dp = [[0 for x in range(n + 1)] for x in range(m + 1)]
+
+    for i in range(m + 1):
+        for j in range(n + 1):
+            if i == 0:
+                dp[i][j] = j
+            elif j == 0:
+                dp[i][j] = i
+            elif str1[i - 1] == str2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
+            else:
+                dp[i][j] = 1 + min(dp[i][j - 1],        # Insert
+                                   dp[i - 1][j],        # Remove
+                                   dp[i - 1][j - 1])    # Replace
+
+    return dp[m][n]
+
+if __name__ == '__main__':
+    str1 = "sunday"
+    str2 = "saturday"
+    print(f"Edit distance between '{str1}' and '{str2}' is {edit_distance(str1, str2, len(str1), len(str2))}")
