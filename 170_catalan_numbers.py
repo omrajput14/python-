d4acc19def1e52d0c9c539892186e5a9121c25d7
Om Rajput <omrajputt369@gmail.com>
2026-07-04 14:49:58 +0530

    Add 170_catalan_numbers.py

---
+++ 170_catalan_numbers.py
@@ -0,0 +1,17 @@
+def catalan_dp(n):
+    if n <= 1:
+        return 1
+
+    catalan = [0 for i in range(n + 1)]
+    catalan[0] = 1
+    catalan[1] = 1
+
+    for i in range(2, n + 1):
+        for j in range(i):
+            catalan[i] += catalan[j] * catalan[i - j - 1]
+
+    return catalan[n]
+
+if __name__ == '__main__':
+    for i in range(10):
+        print(f"Catalan number C({i}) is {catalan_dp(i)}")
