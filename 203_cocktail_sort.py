3e427803258d35d32d889314778b08d00fe0799e
Om Rajput <omrajputt369@gmail.com>
2026-07-10 21:33:38 +0530

    Add 203_cocktail_sort.py (algorithm)

---
+++ 203_cocktail_sort.py
@@ -0,0 +1,23 @@
+def cocktail_sort(a):
+    n = len(a)
+    swapped = True
+    start = 0
+    end = n-1
+    while swapped:
+        swapped = False
+        for i in range(start, end):
+            if a[i] > a[i+1]:
+                a[i], a[i+1] = a[i+1], a[i]
+                swapped = True
+        if not swapped: break
+        swapped = False
+        end = end-1
+        for i in range(end-1, start-1, -1):
+            if a[i] > a[i+1]:
+                a[i], a[i+1] = a[i+1], a[i]
+                swapped = True
+        start = start+1
+    return a
+
+if __name__ == "__main__":
+    print(cocktail_sort([3,1,2]))
