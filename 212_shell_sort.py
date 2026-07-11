841bd6b66cbcdbd1b6412b17534bf755e5fd76a8
Om Rajput <omrajputt369@gmail.com>
2026-07-11 23:27:01 +0530

    Add 212_shell_sort.py (algorithm)

---
+++ 212_shell_sort.py
@@ -0,0 +1,17 @@
+def shellSort(arr):
+    n = len(arr)
+    gap = n // 2
+    while gap > 0:
+        for i in range(gap, n):
+            temp = arr[i]
+            j = i
+            while j >= gap and arr[j - gap] > temp:
+                arr[j] = arr[j - gap]
+                j -= gap
+            arr[j] = temp
+        gap //= 2
+
+if __name__ == "__main__":
+    arr = [12, 34, 54, 2, 3]
+    shellSort(arr)
+    print("Shell Sort:", arr)
