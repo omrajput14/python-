bbdee2dfdd3b7815817da3eb794bb888de644421
Om Rajput <omrajputt369@gmail.com>
2026-07-12 23:21:30 +0530

    Add 224_merge_sorted_array.py (algorithm)

---
+++ 224_merge_sorted_array.py
@@ -0,0 +1,15 @@
+def merge(nums1, m, nums2, n):
+    while m > 0 and n > 0:
+        if nums1[m-1] >= nums2[n-1]:
+            nums1[m+n-1] = nums1[m-1]
+            m -= 1
+        else:
+            nums1[m+n-1] = nums2[n-1]
+            n -= 1
+    if n > 0:
+        nums1[:n] = nums2[:n]
+
+if __name__ == "__main__":
+    nums1 = [1,2,3,0,0,0]
+    merge(nums1, 3, [2,5,6], 3)
+    print(nums1)
