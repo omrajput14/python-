41a138d078a45965c4a08e1dee26d7f362fdefd6
Om Rajput <omrajputt369@gmail.com>
2026-07-12 23:21:41 +0530

    Add 229_linked_list_cycle.py (algorithm)

---
+++ 229_linked_list_cycle.py
@@ -0,0 +1,18 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+def has_cycle(head):
+    slow = fast = head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast: return True
+    return False
+
+if __name__ == "__main__":
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = head
+    print(has_cycle(head))
