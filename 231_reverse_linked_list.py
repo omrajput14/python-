862e80323b0fb05663e966b3d05463294f7391ab
Om Rajput <omrajputt369@gmail.com>
2026-07-12 23:21:46 +0530

    Add 231_reverse_linked_list.py (algorithm)

---
+++ 231_reverse_linked_list.py
@@ -0,0 +1,18 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def reverse_list(head):
+    prev = None
+    curr = head
+    while curr:
+        nxt = curr.next
+        curr.next = prev
+        prev = curr
+        curr = nxt
+    return prev
+
+if __name__ == "__main__":
+    head = ListNode(1, ListNode(2))
+    print(reverse_list(head).val)
